40a028b5aa785e8b8fe7f4e39e1ef0e9f584668f
Léa <lea.lescure@hotmail.fr>
2020-09-22 17:17:44 +0200

    memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,21 +6,21 @@
 /*   By: lea <llescure@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/09 19:13:50 by lea               #+#    #+#             */
-/*   Updated: 2020/09/09 19:18:36 by lea              ###   ########.fr       */
+/*   Updated: 2020/09/22 17:16:37 by lea              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*void	*memset(void *b, int c, unsigned int len)
+void	*ft_memset(void *b, int c, unsigned int len)
 {
-	
-}
-*/
+	unsigned int i;
+	char *temp;
 
-#include <string.h>
-#include <stdio.h>
-
-int		main(void)
-{
-	void *test = NULL;
-	printf("%s\n", memset(test, 'c', 1));
+	i = 0;
+	temp = b;
+	while (i < len)
+	{
+		temp[i] = (unsigned char) c;
+		i++;
+	}
+	return (temp);
 }
