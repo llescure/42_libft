0ee23e39353333a6190cd72c8c6e294b38d7ce90
Léa <lea.lescure@hotmail.fr>
2020-10-30 16:45:32 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 23:20:35 by llescure          #+#    #+#             */
-/*   Updated: 2020/10/25 22:23:12 by llescure         ###   ########.fr       */
+/*   Updated: 2020/10/30 16:45:17 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,10 +29,10 @@ int		ft_count(char const *s, char sep)
 	while (s[i] != '\0')
 	{
 		if (ft_sep(s[i], sep) == 0 && (ft_sep(s[i + 1], sep) == 1 ||
-			   	s[i + 1] == '\0'))
+			s[i + 1] == '\0'))
 			compt++;
 		if (s[i + 1] == '\0')
-			break;
+			break ;
 		i++;
 	}
 	return (compt);
@@ -61,21 +61,20 @@ int		ft_cara(char const *s, char sep, int pos)
 
 char	**ft_split(char const *s, char c)
 {
-	char **tab;
-	int i;
-	int j;
-	int pos;
+	char						**tab;
+	int							i;
+	int							j;
+	int							pos;
 
 	i = 0;
 	pos = 0;
-	if(!(tab = (char **) malloc(sizeof(char) * ft_count(s, c) + 1)))
+	if (!(tab = malloc(sizeof(char *) * (ft_count(s, c) + 1))))
 		return (NULL);
 	while (i < ft_count(s, c))
 	{
 		j = 0;
-		if (!(tab[i] = (char *) malloc(sizeof(char) * ft_cara(s, c, pos) + 1)))
+		if (!(tab[i] = (char *)malloc(sizeof(char) * (ft_cara(s, c, pos) + 1))))
 			return (NULL);
-		printf("i = %i\n", i);
 		while (s[pos] != '\0')
 		{
 			while (ft_sep(s[pos], c) == 0 && s[pos] != '\0')
@@ -87,32 +86,28 @@ char	**ft_split(char const *s, char c)
 			if (j == ft_cara(s, c, pos - j))
 			{
 				pos++;
-				break;
+				break ;
 			}
 			pos++;
 		}
-		//printf("pos = %i\n", pos);	
-		//printf("j = %i\n", j);	
 		tab[i][j] = '\0';
-		//printf("dans split %s\n", tab[i]);
 		i++;
 	}
 	tab[i] = NULL;
-	//printf("%s\n", tab[0]);
 	return (tab);
 }
 
 int		main(void)
 {
-	char test[] = "Coucou les petits potes";
-	char test1 = ' ';
+	char test[] = "                 allo      kikou Coucou les petits potes";
+	char test-1 = ' ';
 	char **print;
 	int i;
 
-	i = 0;
-	print = ft_split(test, test1);
-	printf("%i\n", ft_count(test, test1));
-	while (i <= ft_count(test, test1))
+	i = -2;
+	print = ft_split(test, test-1);
+	printf("nbre mots %i\n", ft_count(test, test-1));
+	while (i <= ft_count(test, test-1))
 	{
 		printf("dans main %d %s\n", i, print[i]);
 		i++;
