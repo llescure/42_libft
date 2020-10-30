b1ba4c203453e31e2090121160349bbaf85c2149
Léa <lea.lescure@hotmail.fr>
2020-10-30 19:10:02 +0100

    libft

--- libft.h
+++ libft.h
@@ -6,11 +6,11 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 20:02:11 by llescure          #+#    #+#             */
-/*   Updated: 2020/10/25 22:09:40 by llescure         ###   ########.fr       */
+/*   Updated: 2020/10/30 19:09:47 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef libft_H
+#ifndef LIBFT_H
 # define LIBFT_H
 
 # include <stdio.h>
@@ -38,7 +38,7 @@ void		*ft_memset(void *b, int c, size_t len);
 char		*ft_strchr(const char *str, int c);
 char		*ft_strdup(const char *s1);
 size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t		ft_strlcpy(char *dest, char *src, size_t destsize);
+size_t		ft_strlcpy(char *dest, const char *src, size_t destsize);
 size_t		ft_strlen(const char *str);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
@@ -50,7 +50,7 @@ int			ft_isalnum(int c);
 int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
-int			ft_isprint(int c);
+int			ft_atoi(const char *str);
 void		ft_bzero(void *s, size_t n);
 
 #endif
