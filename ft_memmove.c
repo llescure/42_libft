a5383063c27ac660644b199a237757e53bba038a
Léa <lea.lescure@hotmail.fr>
2020-11-01 12:26:27 +0100

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,44 +6,33 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 23:17:52 by llescure          #+#    #+#             */
-/*   Updated: 2020/10/29 14:02:14 by llescure         ###   ########.fr       */
+/*   Updated: 2020/11/01 12:25:12 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-unsigned int	ft_strlen2(char *str)
-{
-	unsigned int i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-void			*ft_memmove(void *dst, const void *src, size_t len)
+void			*ft_memmove(void *dest, const void *src, size_t len)
 {
 	char						*tempdest;
 	const char					*tempsrc;
+	char						*tempcpy;
 	unsigned int				i;
 
 	i = 0;
-	tempdest = dst;
+	tempdest = dest;
 	tempsrc = src;
-	if (len > ft_strlen2(tempdest))
-	{
-		while (i < len)
-		{
-			tempdest[i] = tempsrc[i];
-			i++;
-		}
-		tempdest[i] = '\0';
-		return (tempdest);
-	}
+	if (!(tempcpy = malloc(sizeof(char) * (len + 1))))
+			return (NULL);
 	while (i < len)
 	{
-		tempdest[i] = tempsrc[i];
+		tempcpy[i] = tempsrc[i];
+		i++;
+	}
+	i = 0;
+	while (i < len)
+	{
+		tempdest[i] = tempcpy[i];
 		i++;
 	}
 	return (tempdest);
