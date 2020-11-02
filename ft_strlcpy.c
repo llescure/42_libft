8f41ea9aac0b33575fde421e67ebe010b0ba8f56
Léa <lea.lescure@hotmail.fr>
2020-11-02 20:39:44 +0100

    correction strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,22 +6,23 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 23:33:12 by llescure          #+#    #+#             */
-/*   Updated: 2020/11/01 23:02:51 by llescure         ###   ########.fr       */
+/*   Updated: 2020/11/02 20:32:37 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t			ft_strlcpy(char *dest, const char *src, size_t destsize)
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t i;
 
 	i = 0;
-	while (i < destsize && src[i] != '\0')
+	while (i + 1 < dstsize && src[i] != '\0')
 	{
-		dest[i] = src[i];
+		dst[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	if (dstsize != 0)
+		dst[i] = '\0';
 	return (ft_strlen((char *)src));
 }
