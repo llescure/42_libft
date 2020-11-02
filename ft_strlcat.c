746c42e01b3d80cf038ea6f446a6f0368645c5b7
Léa <lea.lescure@hotmail.fr>
2020-11-02 19:18:35 +0100

    with correction

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: llescure <llescure@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 23:32:51 by llescure          #+#    #+#             */
-/*   Updated: 2020/11/01 23:49:26 by llescure         ###   ########.fr       */
+/*   Updated: 2020/11/02 19:15:12 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,15 @@ size_t			ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size = ft_strlen(dst);
 	i = size;
 	j = 0;
-	if (dstsize == 0)
-		return (0);
-	while (i < dstsize - 1)
+	while (i + 1 < dstsize && j < (ft_strlen(src)))
 	{
 		dst[i] = src[j];
-		printf("%c\n", dst[i]);
 		i++;
 		j++;
 	}
-	dst[i] = '\0';
-	if (dstsize <= size)
+	if (!((dstsize == 0) || (size > dstsize)))
+		dst[i] = '\0';
+	if (dstsize < size)
 		return (dstsize + ft_strlen((char *)src));
 	else
 		return (size + ft_strlen((char *)src));
