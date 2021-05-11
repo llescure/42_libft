9f18f58557b969adfe44ec8831d2be8bea4c0d53
Lea Lescure <lea.lescure@hotmail.fr>
2021-05-11 22:37:24 +0200

    ok

--- srcs/double_linked_lists/ft_double_lstdelone.c
+++ srcs/double_linked_lists/ft_double_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: llescure <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 12:16:45 by llescure          #+#    #+#             */
-/*   Updated: 2021/05/11 17:09:34 by llescure         ###   ########.fr       */
+/*   Updated: 2021/05/11 22:37:12 by llescure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 void	ft_double_lstdelone(t_double_list **lst, int pos)
 {
-	if (*lst == NULL || del == NULL)
+	if (*lst == NULL || pos < 0)
 		return ;
 	while (*lst->prev != NULL)
 		*lst = *lst->prev;
 	if (pos == 0)
 		case_first_node(lst);
-	if (pos == ft_double_lstsize(lst))
-		case_last_node(lst, del);
+	else if (pos == ft_double_lstsize(lst))
+		case_last_node(lst);
 }
 
 void	case_first_node(t_double_lst **lst)
 {
 	t_double_list	*temp;
 
-	temp->next = *lst->next;
-	temp->previous = NULL;
-	free(*lst->content);
-	*lst = temp;
+	temp = *lst;
+	*lst = temp->next;;
+	temp->next->prev = temp->next;
+	free(temp);
 }
 
 void	case_last_node(t_double_lst **lst)
@@ -40,8 +40,8 @@ void	case_last_node(t_double_lst **lst)
 
 	while (*lst->next != NULL)
 		*lst = *lst->next;
-	temp->previous = *lst->previous;
-	temp->next = NULL;
-	free(*lst->content);
-	*lst = temp;
+	temp = *lst;
+	*lst = temp->prev;
+	temp->prev->next = temp->prev;
+	free(temp);
 }
